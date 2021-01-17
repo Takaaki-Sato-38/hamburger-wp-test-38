b678145748c7eeb411ddaedb4ceedbf902bc9ae8
Takaaki Sato <sabamaruha@gmail.com>
2021-01-17 15:10:00 +0900

    function.php作成中

---
+++ function.php
@@ -0,0 +1,15 @@
+<php?
+
+function my_setup() {
+  add_theme_support( 'post-thumbnails' ); /* アイキャッチ */
+  add_theme_support( 'automatic-feed-links' ); /* RSSフィード */
+  add_theme_support( 'title-tag' ); /* タイトルタグ自動生成 */
+  add_theme_support( 'html5', array( /* HTML5のタグで出力 */
+    'search-form',
+    'comment-form',
+    'comment-list',
+    'gallery',
+    'caption',
+  ) );
+}
+
--- hamburger.js
+++ hamburger.js
@@ -1,32 +1,32 @@
 
 /* メニューボタンを押すとサイドメニューが出現する */
-$(function() {
-	$('.c-side-menu-btn').click(function() {
-	  $('.side-menu').toggleClass('show');
+jQuery(function() {
+	jQuery('.c-side-menu-btn').click(function() {
+		jQuery('.side-menu').toggleClass('show');
 	});
   });
 
 /* メニューボタンを押すと✖️ボタンが出現 */
-  $(function() {
-	$('.c-side-menu-btn').click(function() {
-		$('.c-fas').toggleClass('show'); 
+jQuery(function() {
+	jQuery('.c-side-menu-btn').click(function() {
+		jQuery('.c-fas').toggleClass('show'); 
 		
 	});
   }); 
 
 /* メニューボタンを押すとメニューボタンが消える */
-  $(function() {
-	$('.c-side-menu-btn').click(function() {
+jQuery(function() {
+	jQuery('.c-side-menu-btn').click(function() {
 		
-		$('.c-side-menu-btn').toggleClass('show');
+		jQuery('.c-side-menu-btn').toggleClass('show');
 	});
   }); 
 
  /* メニューボタンを押すと黒いオーバーレイがボディにかかる */
-  $(function() {
-	$('.c-side-menu-btn').click(function() {
+ jQuery(function() {
+	jQuery('.c-side-menu-btn').click(function() {
 		
-		$('.l-body').toggleClass('side-open');
+		jQuery('.l-body').toggleClass('side-open');
 	});
   }); 
 
@@ -34,9 +34,9 @@ $(function() {
 
 /* ✖️ボタンを押すとサイドメニューが閉じる */ 
 
-  $(function() {
-	$('.c-fas').click(function() {
-	  $('.side-menu').removeClass('show');	  
+jQuery(function() {
+	jQuery('.c-fas').click(function() {
+		jQuery('.side-menu').removeClass('show');	  
 	});
   });
 
@@ -48,17 +48,17 @@ $(function() {
   });
 
 /* ✖️ボタンを押すとメニューボタンが出現する */
-  $(function() {
-	$('.c-fas').click(function() {
-	  $('.c-side-menu-btn').removeClass('show');
+jQuery(function() {
+	jQuery('.c-fas').click(function() {
+		jQuery('.c-side-menu-btn').removeClass('show');
 	  
 	});
   });
 
 /* ✖️ボタンを押すとオーバーレイが消える*/  
-  $(function() {
-	$('.c-fas').click(function() {
-	  $('.l-body').removeClass('side-open');
+jQuery(function() {
+	jQuery('.c-fas').click(function() {
+		jQuery('.l-body').removeClass('side-open');
 	  
 	});
   });
