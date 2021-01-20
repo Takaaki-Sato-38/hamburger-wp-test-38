fdae95aea1f4900e4d78dd92f59c35cab6f87656
Takaaki Sato <sabamaruha@gmail.com>
2021-01-20 21:30:38 +0900

    CSS 外部フォント　JS　読み込み

--- function.php
+++ function.php
@@ -13,3 +13,14 @@ function my_setup() {
   ) );
 }
 
+function register_stylesheet() { //読み込むCSSを登録する
+	wp_register_style('ress', get_template_directory_uri().'/ress.css');
+	wp_register_style('style', get_template_directory_uri().'/style.css');
+  wp_register_style('style', get_template_directory_uri().'/css/stylehbg.css');
+}
+wp_enqueue_script('google-fonts_script', 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700;900&display=swap'); 
+wp_enqueue_script('google-fonts_script', 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap');
+wp_enqueue_script('google-fonts_script', 'https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@400;700;900&display=swap');
+wp_enqueue_script('fontawesome_script', 'https://use.fontawesome.com/releases/v5.6.1/css/all.css');
+
+wp_enqueue_script('hamburger_script', get_template_directory_uri() . '/hamburger.js', array(jQuery), '', true); 
