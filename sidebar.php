50609fe2768ad876e02cc67146e0ffb4bd3e3daf
Takaaki Sato <sabamaruha@gmail.com>
2021-01-24 15:12:10 +0900

    responsive-sidemenu スタイリング済み

--- sidebar.php
+++ sidebar.php
@@ -10,30 +10,8 @@
 
         <!-- サイドメニュー -->
         <nav class="side-menu">
-    
-            <ul class="l-side-hamburger c-menu">バーガー
-                <li>ハンバーガー</li>
-                <li>チーズバーガー</li>
-                <li>テリヤキバーガー</li>
-                <li>アボカドバーガー</li>
-                <li>フィッシュバーガー</li>
-                <li>ベーコンバーガー</li>
-                <li>チキンバーガー</li>
-            </ul>
-            <ul class="l-side-side-menu c-menu">サイド
-                <li>ポテト</li>
-                <li>サラダ</li>
-                <li>ナゲット</li>
-                <li>コーン</li>
-            </ul>
-            <ul class="l-side-drink-menu c-menu">ドリンク
-                <li>コーラ</li>
-                <li>ファンタ</li>
-                <li>オレンジ</li>
-                <li>アップル</li>
-                <li>紅茶 (Ice/Hot)</li>
-                <li>コーヒー (Ice/Hot)</li>
-            </ul>
+            <?php wp_nav_menu(); ?>
+            
 
         </nav>
         
