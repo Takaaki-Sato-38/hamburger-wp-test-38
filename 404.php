f07bb4df6b629530ae488658c761fe92411ca16e
Takaaki Sato <sabamaruha@gmail.com>
2021-02-07 20:28:15 +0900

    404ページ作成

---
+++ 404.php
@@ -0,0 +1,26 @@
+  
+  <?php get_header(); ?>
+  
+  <!-- トップ画像② -->
+  <div class="l-header-top-image c-image">
+      <h2 class="c-image-text-top">Not found</h2>
+      
+  </div>
+  <!-- 404 -->
+  
+  <div class="l-header-choice c-404-wrapper">
+    <p>お探しのページが見つかりませんでした。</p>
+    <p>申し訳ございませんが、<a class="c-text-404" href="<?php echo home_url('/'); ?>">こちらのリンク</a>からトップページにお戻りください。</p>
+      
+  </div>
+  
+</div>
+
+
+<?php get_sidebar(); ?>
+
+
+
+</div>
+
+<?php get_footer(); ?>
